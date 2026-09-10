3d008562bccea8c33a8d03860c0cb8701d2553af
Sakiih <rasyaakml136@gmail.com>
2026-09-10 19:55:54 +0700

    Add files via upload

---
+++ Praktikum-Mingguan-01/LuasPersegiPanjang.php
@@ -0,0 +1,18 @@
+<?php
+//PROSEDURAL
+function luasPersegiPanjang($panjang, $lebar) 
+{
+    return $panjang * $lebar;
+}
+
+echo "<h3>PROGRAM PROSEDURAL MENGHITUNG LUAS PERSEGI PANJANG</h3>";
+echo "Panjang = 10<br>";
+echo "Lebar = 5<br><br>";
+echo "Rumus = Panjang × Lebar<br>";
+
+echo "Luas Persegi Panjang adalah : " . luasPersegiPanjang(10, 5) . "<br>";
+
+?>
+
+
+
